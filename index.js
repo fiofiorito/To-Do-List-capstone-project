7f004b8e1aaeedc14367e1f0f169b3dec5c2ea9e
Fiofi Fiorito <fiofi.fiorito@gmail.com>
2023-07-25 16:42:24 -0300

    upload completed to do list

---
+++ index.js
@@ -0,0 +1,90 @@
+import 'dotenv/config';
+import express from "express";
+import bodyParser from "body-parser";
+import _ from "lodash";
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0.ze4y6ft.mongodb.net/?retryWrites=true&w=majority");
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+const Item = mongoose.model('Item', itemsSchema);
+
+// const listSchema = new mongoose.Schema({
+//     name: String,
+//     items: [itemsSchema]
+// });
+
+// const List = mongoose.model('List', listSchema);
+
+const item1 = new Item({
+    name: "Welcome to your to do list!"
+});
+
+const item2 = new Item({
+    name: "Click '+' to add a task"
+});
+
+const item3 = new Item({
+    name: "To delete check the box!"
+});
+
+const defaultItems = [item1, item2, item3]
+
+app.get("/", (req, res) => {
+    Item.find()
+        .then((items) => {
+            if (items.length === 0) {
+                Item.insertMany(defaultItems)
+                    .then((err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("success");
+                        }
+                    });
+                res.redirect("/");
+            } else {
+                console.log(items.length);
+                res.render("index.ejs", { newListItems: items, itemName: req.body.newItem });
+            }
+        });
+});
+
+app.post("/", (req, res) => {
+    const itemName = req.body.newItem;
+    const newItem = new Item({
+        name: itemName
+    });
+    newItem.save();
+    res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const checkedItem = req.body.checkbox;
+    Item.deleteOne({ _id: checkedItem })
+        .then(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("success");
+            }
+        });
+    res.redirect("/");
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`port ${port} up and running!`);
+})
---
+++ readME.md
@@ -0,0 +1,15 @@
+# To Do List
+Welcome to my to do list!!
+
+## Overview
+I created a to do list from scratch without following any tutorials. This was a challenge I set up for myself, I wanted to have more personal projects that did not come from a course or bootcamp. So here it is another project of mine!!
+
+## Steps I took to achieve this
+1. Created a wireframe
+3. Created get route for index.ejs
+2. Styled the ejs files
+3. Added a mongoDB database to the index.js file
+4. Created post routes for index.ejs
+5. Did CRUD (create, read and delete, no update) on the list items that are stored in the db
+6. Tested to see if everything works
+7. Styled the responsiveness for tablet and laptop viewports (originally mobile responsive)
